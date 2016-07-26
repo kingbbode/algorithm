d2f64e5b498f9744732b64f6e0fd36971c992095
kingbbode <kingbbode@gmail.com>
2016-07-27 00:16:13 +0900

    2차 개선 : 입력 방식 변경

--- src/problem1021/Main.java
+++ src/problem1021/Main.java
@@ -1,27 +1,30 @@
 package problem1021;
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
-        int M = scan.nextInt();
-        ArrayList<Integer> l = new ArrayList<>();
-        for (int i = 0; i < M; i++) {
-            l.add(scan.nextInt());
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         ArrayList<Integer> q = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             q.add(i + 1);
         }
+        st = new StringTokenizer(in.readLine());
         int now = 0;
         int move = 0;
-        for (int i : l) {
+        for (int i = 0; i < M; i++) {
+            int index = Integer.parseInt(st.nextToken());
             int right;
             int left;
             int s = q.size();
-            int x = q.indexOf(i);
+            int x = q.indexOf(index);
             if (x >= now) {
                 right = x - now;
                 left = s - right;
