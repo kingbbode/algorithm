48fddc6586fb01df2aac4775f75e65000d318ee9
kingbbode <kingbbode@gmail.com>
2016-07-27 00:51:34 +0900

    리펙토링

--- src/problem1021/Main.java
+++ src/problem1021/Main.java
@@ -1,47 +1,27 @@
 package problem1021;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(in.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+        int now = 0;
+        int move = 0;
         ArrayList<Integer> q = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             q.add(i + 1);
         }
         st = new StringTokenizer(in.readLine());
-        int now = 0;
-        int move = 0;
         for (int i = 0; i < M; i++) {
-            int index = Integer.parseInt(st.nextToken());
-            int right;
-            int left;
-            int s = q.size();
-            int x = q.indexOf(index);
-            if (x >= now) {
-                right = x - now;
-                left = s - right;
-            } else {
-                left = now - x;
-                right = s - left;
-            }
-            if (right <= left) {
-                now += right;
-                move += right;
-                if (now >= s) now = now - s;
-            } else {
-                now -= left;
-                move += left;
-                if (now < 0) now = s + now;
-            }
-            q.remove(now);
+            int x = q.indexOf(Integer.parseInt(st.nextToken()));
+            int m = Math.abs(x - now);
+            move += Math.min(m, q.size() - m);
+            q.remove(x);
+            now = x;
         }
         System.out.println(move);
     }
