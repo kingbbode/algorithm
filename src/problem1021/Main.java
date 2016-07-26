35d7093c5bf296e5b23121f4d8603036c2bd9449
kingbbode <kingbbode@gmail.com>
2016-07-26 23:19:09 +0900

    문제 잘못이해하여 다시 품

--- src/problem1021/Main.java
+++ src/problem1021/Main.java
@@ -7,68 +7,31 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
         int M = scan.nextInt();
-        Set<Integer> list = new HashSet<>();
+        ArrayList<Integer> l = new ArrayList<>();
         for (int i = 0; i < M; i++) {
-            list.add(scan.nextInt());
+            l.add(scan.nextInt());
         }
 
         ArrayDeque<Integer> q = new ArrayDeque<>();
         for (int i = 0; i < N; i++) {
             q.add(i + 1);
         }
-
-        int goal = M;
         int move = 0;
-        while (goal > 0) {
-            int cntDouble = -1;
-            int cntLeft = 0;
-            int cntRight = 0;
-            Iterator<Integer> q0 = q.iterator();
-            boolean before = false;
-            while (q0.hasNext()) {
-                boolean now = list.contains(q0.next());
-                if(before && now){
-                    break;
-                }
-                before = now;
-                cntDouble++;
+        for (int i : l) {
+            int cnt = 0;
+            for (int j : q) {
+                if (j == i) break;
+                cnt++;
             }
-            Iterator<Integer> q1 = q.iterator();
-            while (q1.hasNext()) {
-                if (list.contains(q1.next())) {
-                    break;
-                }
-                cntLeft++;
+            int s = q.size();
+            boolean isLeft = (s / 2) >= cnt;
+            int c = isLeft ? cnt : s - cnt;
+            for (int k = 0; k < c; k++) {
+                if (isLeft) q.addLast(q.pop());
+                else q.addFirst(q.pollLast());
             }
-
-            Iterator<Integer> q2 = q.descendingIterator();
-            while (q2.hasNext()) {
-                if (list.contains(q2.next())) {
-                    break;
-                }
-                cntRight++;
-            }
-            if (cntDouble <= cntLeft) {
-                while (cntDouble > 0) {
-                    q.addLast(q.pop());
-                    move++;
-                    cntDouble--;
-                }
-            } else if (cntLeft <= cntRight) {
-                while (cntLeft > 0) {
-                    q.addLast(q.pop());
-                    move++;
-                    cntLeft--;
-                }
-            } else {
-                while (cntRight >= 0) {
-                    q.addFirst(q.pollLast());
-                    move++;
-                    cntRight--;
-                }
-            }
+            move += c;
             q.pop();
-            goal--;
         }
         System.out.println(move);
     }
